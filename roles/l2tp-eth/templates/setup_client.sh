3f57950d28dbc64c7063d64021d666122f8e5afe
Tao Fei <filia.tao@gmail.com>
2015-08-02 12:54:33 +0800

    working l2th-eth static tunnel

--- roles/l2tp-eth/templates/setup_client.sh
+++ roles/l2tp-eth/templates/setup_client.sh
@@ -5,11 +5,13 @@ SERVER_IP="{{ l2tp_eth_client_remote_ip }}"
 SERVER_PORT="{{ l2tp_eth_client_remote_port }}"
 LOCAL_IP="{{ l2tp_eth_client_local_ip }}"
 PEER_IP="{{ l2tp_eth_client_peer_ip }}"
-ETH_NAE="{{ l2tp_eth_name }}"
+ETH_NAME="{{ l2tp_eth_name }}"
 MTU="{{ l2tp_eth_mtu }}"
 SESSION="0x{{ l2tp_eth_session }}"
 COOKIE="{{ l2tp_eth_cookie }}"
 
+modprobe l2tp_eth
+ip l2tp del tunnel tunnel_id 1
 ip l2tp add tunnel local $CLIENT_IP remote $SERVER_IP tunnel_id 1 peer_tunnel_id 1 encap udp udp_sport $CLIENT_PORT udp_dport $SERVER_PORT
 ip l2tp add session tunnel_id 1 session_id $SESSION peer_session_id $SESSION cookie $COOKIE peer_cookie $COOKIE
 ip addr add $LOCAL_IP peer $PEER_IP dev $ETH_NAME
